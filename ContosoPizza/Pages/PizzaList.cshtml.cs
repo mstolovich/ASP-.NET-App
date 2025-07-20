92e6c1409d42ca29002bcd08a62a3bebdd1486d8
mstolovich <mstolovych@gmail.com>
2025-07-20 19:22:30 +0200

    Add pizza creation and deletion functionality to PizzaList page

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -6,18 +6,40 @@ using ContosoPizza.Services;
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
-{
-    private readonly PizzaService _service;
-    public IList<Pizza> PizzaList { get;set; } = default!;
-
-    public PizzaListModel(PizzaService service)
     {
-        _service = service;
-    }
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
 
-    public void OnGet()
-    {
-        PizzaList = _service.GetPizzas();
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+        }
+
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
+
+        public void OnGet()
+        {
+            PizzaList = _service.GetPizzas();
+        }
     }
-}
 }
